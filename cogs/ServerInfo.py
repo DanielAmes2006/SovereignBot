4463d4db30b679defda9780edb454f1f088a2b4c
DanielAmes2006 <daa94@aber.ac.uk>
2025-04-21 16:58:47 +0100

    Update ServerInfo.py

--- cogs/ServerInfo.py
+++ cogs/ServerInfo.py
@@ -4,7 +4,7 @@ from discord import app_commands
 import json
 import os
 
-SERVER_INFO_FILE = "server_info.json"
+SERVER_INFO_FILE = os.path.join(os.path.dirname(__file__), "..", "server_info.json")
 
 def load_server_info():
     """ Loads server configuration from JSON file """
