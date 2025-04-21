72f332f3092451844c5383c16f4fedec350fc734
DanielAmes2006 <daa94@aber.ac.uk>
2025-04-21 16:57:28 +0100

    Update Deployments.py

--- cogs/Deployments.py
+++ cogs/Deployments.py
@@ -5,7 +5,7 @@ import time
 import json
 import os
 
-SERVER_INFO_FILE = os.path.expanduser("~/Sovereignbot/server_info.json")
+SERVER_INFO_FILE = os.path.join(os.path.dirname(__file__), "..", "server_info.json")
 
 def load_server_info():
     """ Loads server configuration from JSON file """
